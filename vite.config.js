f1b43a9c88b65deee02020a21516e297cbd692af
Ravi22110219 <ravi.kumawat@iitgn.ac.in>
2024-12-12 12:02:32 +0530

    some changes

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: 'https://airesqclimsols.com',  // Match with homepage in package.json
+  base: 'https://Ravi22110219.github.io/FloodAIRepo/',  // Match with homepage in package.json
   build: {
     outDir: 'dist',  // Ensure the build output directory is correct
   },
